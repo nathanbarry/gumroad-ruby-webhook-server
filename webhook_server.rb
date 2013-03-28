cd3f0f1291d0bc57dc95180b959f8c8eeb9d2534
Nathan Barry <nathan@thinklegend.com>
2013-03-27 19:44:34 -0600

    added error messages

--- webhook_server.rb
+++ webhook_server.rb
@@ -19,12 +19,18 @@ post '/gumroad-webhook' do
     :license_key => license_key
   )
   "http://www.license-key.com/this-file/#{license_key}"
+  rescue => e
+    "Error Occured"
+  end
 end
 
 get '/this-file/:license_key' do |key|
   entry = Entry.find_by_license_key(key)
   return "Invalid Key" unless entry.present?
   send_file('download_file.txt')
+  rescue => e
+    "Error Occured"
+  end
 end
 
 class Entry < ActiveRecord::Base
